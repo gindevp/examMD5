9a7f4b392dcdaeca4dff162764f25020bac9fbf3
quyet <=>
2022-10-29 21:35:01 +0700

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://localhost:3000",
+  firebaseConfig: {
+    apiKey: "AIzaSyBt2cY4kErVpV-FCA5ZRx3gRTe_k4vknV0",
+    authDomain: "angularapp-741db.firebaseapp.com",
+    projectId: "angularapp-741db",
+    storageBucket: "angularapp-741db.appspot.com",
+    messagingSenderId: "1045847684942",
+    appId: "1:1045847684942:web:66e0c47bfa53aae500101f",
+    measurementId: "G-CM087QRHBV"
+  }
 };
